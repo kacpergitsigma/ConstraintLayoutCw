efe506d1359107d439742090470dd8e769b75215
kacpergitsigma <kacper.git9083@gmail.com>
2025-12-16 08:31:02 +0100

    dodałem viewmodel na punkty

--- app/src/main/java/com/pt2/myapplication/MainActivity.java
+++ app/src/main/java/com/pt2/myapplication/MainActivity.java
@@ -8,27 +8,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.pt2.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-ActivityMainBinding binding; // klasa z bindowanymi widokami
+    ActivityMainBinding binding; // klasa z bindowanymi widokami
     // od activity_main.xml
-    private int score = 0;
+    ScoreViewModel scoreViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        binding.textView.setText("0");
+        scoreViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
+
+
+        binding.textView.setText(scoreViewModel.getScore() + "");
 
         binding.button1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        score += 1;
-                        binding.textView.setText(score + "");
+                        scoreViewModel.addScore(1);
+                        binding.textView.setText(scoreViewModel.getScore() + "");
                     }
                 }
         );
@@ -36,8 +41,8 @@ ActivityMainBinding binding; // klasa z bindowanymi widokami
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        score += 2;
-                        binding.textView.setText(score + "");
+                        scoreViewModel.addScore(2);
+                        binding.textView.setText(scoreViewModel.getScore() + "");
                     }
                 }
         );
@@ -45,8 +50,8 @@ ActivityMainBinding binding; // klasa z bindowanymi widokami
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        score += 3;
-                        binding.textView.setText(score + "");
+                        scoreViewModel.addScore(3);
+                        binding.textView.setText(scoreViewModel.getScore() + "");
                     }
                 }
         );
--- app/src/main/java/com/pt2/myapplication/ScoreViewModel.java
+++ app/src/main/java/com/pt2/myapplication/ScoreViewModel.java
@@ -1,5 +1,18 @@
 package com.pt2.myapplication;
 
-public class ScoreViewModel {
+import androidx.lifecycle.ViewModel;
 
+public class ScoreViewModel extends ViewModel {
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void addScore(int amountToAdd) {
+        this.score += amountToAdd;
+    }
 }
